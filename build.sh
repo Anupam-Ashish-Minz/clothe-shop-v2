2d9e0b5b48a4231779d42b17dcb5cec33f7145f6
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-02-22 12:13:12 +0530

    modify build.sh
    use concurrently

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-air &
-cd templates
-nodemon -x "templ generate" -e "templ" &
-tailwindcss -i input.css -o output.css --watch
+# air &
+# cd templates
+# nodemon -x "templ generate" -e "templ" &
+# tailwindcss -i input.css -o output.css --watch
+
+concurrently --kill-others "air"\
+	"cd templates && nodemon -x 'templ generate' -e templ"\
+	"cd templates && tailwindcss -i input.css -o output.css --watch"
