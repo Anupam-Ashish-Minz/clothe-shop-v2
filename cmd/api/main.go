371fb8c68eedf3b871e3531b2c6ccf52182d79c6
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-02-22 11:10:30 +0530

    first

---
+++ cmd/api/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"clothe-shop-v2/internal/server"
+)
+
+func main() {
+
+	server := server.NewServer()
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic("cannot start server")
+	}
+}
