5fc5bcf63e534032227f213cb87cbbaca1f62d32
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-03-19 14:28:09 +0530

    move product function to different file

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -44,30 +43,6 @@ func (s *Server) RegisterRoutes() http.Handler {
 	return r
 }
 
-func (s *Server) FetchProducts(c *gin.Context) {
-	page, err := strconv.Atoi(c.Query("page"))
-	if err != nil {
-		log.Println(err)
-		page = 0
-	}
-	products, err := s.db.GetProducts(page)
-	if err != nil {
-		log.Println(err)
-		c.String(http.StatusInternalServerError, "failed to fetch products")
-		return
-	}
-	if len(products) < 1 {
-		c.String(http.StatusBadRequest, "there are no products in this page")
-		return
-	}
-	err = templates.Products(products, page+1).Render(context.Background(), c.Writer)
-	if err != nil {
-		log.Println(err)
-		c.String(http.StatusInternalServerError, "failed to load products")
-		return
-	}
-}
-
 func (s *Server) HomePage(c *gin.Context) {
 	err := templates.Index().Render(context.Background(), c.Writer)
 	if err != nil {
@@ -77,43 +52,6 @@ func (s *Server) HomePage(c *gin.Context) {
 
 func (s *Server) healthHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, s.db.Health())
-}
-
-func (s *Server) ProductsPage(c *gin.Context) {
-	products, err := s.db.GetProducts(0)
-	if err != nil {
-		log.Println(err)
-		c.String(http.StatusInternalServerError, "failed to fetch products")
-		return
-	}
-	if len(products) < 1 {
-		c.String(http.StatusBadRequest, "no products found")
-		return
-	}
-	err = templates.ProductsPage(products).Render(context.Background(), c.Writer)
-	if err != nil {
-		log.Println(err)
-		c.String(http.StatusInternalServerError, "unable to render template")
-	}
-}
-
-func (s *Server) ProductPage(c *gin.Context) {
-	productID, err := strconv.Atoi(c.Param("id"))
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	product, err := s.db.GetProductById(int64(productID))
-	if err != nil {
-		log.Println(err)
-		c.String(http.StatusInternalServerError, "failed to fetch products")
-		return
-	}
-	log.Println(product)
-	// err = templates.Product(product).Render(context.Background(), c.Writer)
-	// if err != nil {
-	// 	log.Println(err)
-	// }
 }
 
 func (s *Server) AdminPage(c *gin.Context) {
