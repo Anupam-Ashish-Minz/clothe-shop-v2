d8192c76ab75939ffaa616d0fd1280b7a211651d
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-04-13 12:27:36 +0530

    build script updated

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@
 # tailwindcss -i input.css -o output.css --watch
 
 concurrently --kill-others "air"\
-	"cd templates && nodemon -x 'templ generate' -e templ"\
+	"cd templates && templ generate -watch"\
 	"cd templates && tailwindcss -i input.css -o output.css --watch"
