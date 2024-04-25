d877f5207fc40886b26fd63aa1e8e886799e6cbc
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-04-25 15:33:43 +0530

    similar user refactor

--- internal/database/users.go
+++ internal/database/users.go
@@ -3,16 +3,18 @@ package database
 import "fmt"
 
 type User struct {
-	ID       int64
-	Name     string
-	Email    string
-	Password []byte
+	ID       int64  `db:"id"`
+	Name     string `db:"name"`
+	Email    string `db:"email"`
+	Password []byte `db:"password"`
 }
 
 func (s *service) GetUserByEmail(email string) (User, error) {
 	var user User
-	row := s.db.QueryRow(`select id, name, email, password from "User" where email = $1`, email)
-	row.Scan(&user.ID, &user.Name, &user.Email, &user.Password)
+	err := s.db.Get(&user, `select id, name, email, password from "User" where email = $1`, email)
+	if err != nil {
+		return User{}, err
+	}
 	if user.ID == 0 || user.Email == "" {
 		return user, fmt.Errorf("user not found")
 	}
@@ -23,10 +25,9 @@ func (s *service) AddNewUser(user User) (int64, error) {
 	if user.Name == "" || user.Email == "" || user.Password == nil || len(user.Password) == 0 {
 		return 0, fmt.Errorf("missing fields in user object")
 	}
-	row := s.db.QueryRow(`insert into "User" (name, email, password) values ($1, $2, $3) returning id`,
-		user.Name, user.Email, user.Password)
 	var userID int64
-	err := row.Scan(&userID)
+	err := s.db.Get(&userID, `insert into "User" (name, email, password) values ($1, $2, $3) returning id`,
+		user.Name, user.Email, user.Password)
 	if err != nil {
 		return 0, err
 	}
@@ -35,10 +36,9 @@ func (s *service) AddNewUser(user User) (int64, error) {
 
 func (s *service) GetUserById(userID int64) (User, error) {
 	var user User
-	row := s.db.QueryRow(`select id, name, email, password from "User" where id = $1`, userID)
-	err := row.Scan(&user.ID, &user.Name, &user.Email, &user.Password)
+	err := s.db.Get(&user, `select id, name, email, password from "User" where id = $1`, userID)
 	if err != nil {
-		return user, err
+		return User{}, err
 	}
 	if user.ID == 0 || user.Email == "" {
 		return user, fmt.Errorf("user not found")
