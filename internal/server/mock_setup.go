8e0a9cef3a9929392e869df860448c748a4234b7
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-07-20 17:24:49 +0530

    read csv setup

--- internal/server/mock_setup.go
+++ internal/server/mock_setup.go
@@ -2,40 +2,69 @@ package server
 
 import (
 	"encoding/csv"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
 
 	"clothe-shop-v2/internal/database"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/joho/godotenv"
 )
 
-func readCsv() {
+type Product struct {
+	ID          int64  `json:"id,omitempty" db:"id"`
+	Name        string `json:"name,omitempty" db:"name"`
+	Description string `json:"description,omitempty" db:"description"`
+	Price       int    `json:"price,omitempty" db:"price"`
+	Gender      string `json:"gender,omitempty" db:"gender"`
+}
+
+func readCsv() error {
 	file, err := os.Open("../../data/csv/products.csv")
 	r := csv.NewReader(file)
-	products := make([]database.Product, 0)
+	products := make([]Product, 0)
 
 	record, err := r.Read()
-	if !(record[0] == "name" && record[1] == "description" && record[2] == "price" && record[3] == "gender") {
-		// handler it later
+	if !(record[0] == "name" && record[1] == "description" && record[2] == "price" && record[3] == "gender" && len(record) == 4) {
+		return fmt.Errorf("unexpected field names")
 	}
 	for {
 		record, err = r.Read()
 		if err != nil {
 			break
 		}
-		var product database.Product
+		var product Product
 		product.Name = record[0]
 		product.Description = record[1]
 		price, err := strconv.ParseFloat(record[2], 32)
 		product.Price = int(price)
 		if err != nil {
-			// handle it later
+			return err
 		}
 		product.Gender = record[3]
 		products = append(products, product)
 	}
+	conn := os.Getenv("DB_URL_MOCK")
+	db, err := sqlx.Connect("postgres", conn)
+	if err != nil {
+		return err
+	}
+	tx := db.MustBegin()
+	defer tx.Rollback()
+	for _, product := range products {
+		_, err = tx.NamedExec(`INSERT INTO "Product" (id, name, description, price, gender) VALUES (:id, :name, :description, :price, :gender)`, product)
+		if err != nil {
+			return err
+		}
+	}
+	err = tx.Commit()
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
 }
 
 func setupTesting() (*Server, error) {
