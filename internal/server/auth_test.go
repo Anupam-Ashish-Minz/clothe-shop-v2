5ffd5d07121855b0d8073b31596d04c988290c26
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-08-06 11:44:19 +0530

    new auth tests

--- internal/server/auth_test.go
+++ internal/server/auth_test.go
@@ -1,8 +1,35 @@
 package server
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateToken(t *testing.T) {
+	assert := assert.New(t)
+	userID := int64(155)
+	secret := []byte("abcd")
+	tokenString, err := createToken(userID, secret)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxNTV9.nDzTEPrdzRwxhHnMnV0quPhKSjUqrRBwnxAeq7GN3Nw", tokenString)
+}
+
+func TestParseToaken(t *testing.T) {
+	assert := assert.New(t)
+	secret := []byte("abcd")
+	tokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxNTV9.nDzTEPrdzRwxhHnMnV0quPhKSjUqrRBwnxAeq7GN3Nw"
+	parsedUserID, err := parseToken(tokenString, secret)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(int64(155), parsedUserID)
+}
 
 func TestJwtTokens(t *testing.T) {
+	assert := assert.New(t)
 	userID := int64(155)
 	secret := []byte("thisisthekey")
 	tokenString, err := createToken(userID, secret)
@@ -13,8 +40,5 @@ func TestJwtTokens(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	if parsedUserID != userID {
-		t.Fatal("inputs and outputs don't match")
-	}
+	assert.Equal(userID, parsedUserID)
 }
